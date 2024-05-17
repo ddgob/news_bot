43b42cb8274b057eb812b519c4a787bdb3e30787
Daniel Barbosa <danielolibar@gmail.com>
2024-05-17 11:16:46 -0300

    Added feature to close browser

--- LATimesScraper.py
+++ LATimesScraper.py
@@ -5,6 +5,13 @@ class LATimesScraper:
     def __init__(self):
         self.browser = Selenium()
 
+    def close_browser(self):
+        try:
+            self.browser.close_all_browsers()
+            print('Browser closed')
+        except Exception as e:
+            print(f"An error occurred closing the browser: {e}")
+    
     def open_webiste(self):
         try:
             self.browser.open_available_browser('https://www.latimes.com/')
@@ -14,6 +21,7 @@ class LATimesScraper:
 
     def run(self):
         self.open_webiste()
+        self.close_browser()
 
 if __name__ == '__main__':
     scraper = LATimesScraper()
