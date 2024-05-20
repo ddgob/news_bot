6de61b60f6c4c00c1e1bd572e31da23d46994266
Daniel Barbosa <danielolibar@gmail.com>
2024-05-20 13:55:09 -0300

    Create main.py
    - Created main.py at the project root to serve as the entry point for the application
    - Moved the main execution block (if __name__ == "__main__":) from LATimesScraper.py to main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+from news_bot import LATimesScraper
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="LATimesScraper Command Line Arguments")
+    parser.add_argument('-s', '--search_phrase', type=str, required=True, help='Search phrase')
+    parser.add_argument('-e', '--excel_dir', type=str, required=True, help='Directory for Excel files')
+    parser.add_argument('-i', '--image_dir', type=str, required=True, help='Directory for article images')
+    parser.add_argument('-l', '--log_dir', type=str, required=True, help='Directory for log files')
+    parser.add_argument('-sd', '--start_date', type=str, required=True, help='Start date (MM/DD/YYYY)')
+    parser.add_argument('-ed', '--end_date', type=str, required=True, help='End date (MM/DD/YYYY)')
+    args = parser.parse_args()
+    scraper = LATimesScraper(
+        phrase=args.search_phrase, 
+        excel_files_dir=args.excel_dir, 
+        article_images_dir=args.image_dir, 
+        log_dir=args.log_dir, 
+        start_date=args.start_date, 
+        end_date=args.end_date
+    )
+    scraper.run()
