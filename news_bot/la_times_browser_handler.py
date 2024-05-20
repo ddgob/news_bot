560ee8cce852a0d4553ab09c9657c5cc60029411
Daniel Barbosa <danielolibar@gmail.com>
2024-05-20 14:52:00 -0300

    Add open website functionality to LATimesBrowserHandler
    - Added funcitonality
    - Will substitute corresponding functionality in LATimesScraperClass with it

--- news_bot/la_times_browser_handler.py
+++ news_bot/la_times_browser_handler.py
@@ -8,3 +8,11 @@ class LATimesBrowserHandler:
     def __init__(self, log_dir):
         self.__la_times_browser_handler = Selenium()
         self.__logger = Logger(log_dir).log
+
+    def open_website(self):
+        try:
+            self.__logger('info', f'Opening {self.__website_url} website...')
+            self.__la_times_browser_handler.open_available_browser('https://www.latimes.com/')
+            self.__logger('info', f'Finished opening {self.__website_url} website')
+        except Exception as e:
+            self.logger('error', f'An error occurred while opening the {self.__website_url} website: {e}')
