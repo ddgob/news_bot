de97ae427bb37b98d4c2cc216db4c9555b0fb470
Daniel Barbosa <danielolibar@gmail.com>
2024-05-21 17:46:08 -0300

    refactor: Update Logger class to use config for log directory and improve code readability
    - Changed the singleton implementation to use private  __instance instead of pseudo protected _instance.
    - Updated the Logger class to use the log directory from the config module.
    - Renamed the logger to 'news_bot_logger'.
    - Modified the date format in the log file name to a more readable date format.
    - Improved code readability by reformatting long lines.
    - Refactored the warning handler setup for better clarity.

--- news_bot/logger.py
+++ news_bot/logger.py
@@ -1,35 +1,44 @@
 import logging
 from datetime import datetime, timedelta
 import warnings
+from . import config
 
 class Logger:
-    
-    _instance = None
+    __instance = None
 
-    def __new__(cls, log_dir):
-        if cls._instance is None:
-            cls._instance = super(Logger, cls).__new__(cls)
-            cls._instance._initialize_logger(log_dir)
-        return cls._instance
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(Logger, cls).__new__(cls)
+            cls.__instance._initialize_logger()
+        return cls.__instance
     
-    def _initialize_logger(self, log_dir):
-        self.logger = logging.getLogger('LATimesScraperLogger')
+    def _initialize_logger(self):
+        self.logger = logging.getLogger('news_bot_logger')
         self.logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(f'{log_dir}/LATimesScraper_{datetime.now().strftime("%Y%m%d%H%M%S")}.log')
+        handler = logging.FileHandler(
+            f'{config.LOG_FILE_DIR}/news_bot_logger_'
+            f'{datetime.now().strftime("%m-%d-%Y_%H:%M:%S")}.log'
+        )
         handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
         # Redirect warnings to the logger
         logging.captureWarnings(True)
         warn_handler = logging.StreamHandler()
         warn_handler.setLevel(logging.WARNING)
-        warn_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        warn_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
         warn_handler.setFormatter(warn_formatter)
         self.logger.addHandler(warn_handler)
 
-        def warn_with_logger(message, category, filename, lineno, file=None, line=None):
-            self.logger.warning(warnings.formatwarning(message, category, filename, lineno, line))
+        def warn_with_logger(message, category, filename, lineno, file=None, 
+                             line=None):
+            self.logger.warning(warnings.formatwarning(message, category, 
+                                                       filename, lineno, line))
 
         warnings.showwarning = warn_with_logger
 
