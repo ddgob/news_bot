5895c880188cd97c49a65b050828b0fe2e4a157b
Daniel Barbosa <danielolibar@gmail.com>
2024-05-21 16:30:39 -0300

    feat: Create Article class with methods for handling article attributes
    - Implemented __init__ method to initialize Article objects with title, date, description, and image source attributes.
    - Added getter methods: get_title, get_date, get_description, and get_image_src.
    - Added methods for date comparison: is_before_date, and is_between_dates.
    - Implemented get_image_file_name method to extract image file name from image source.
    - Added is_contain_money_in_text method to check if the title or description contains monetary values.

---
+++ news_bot/article.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from .url_handler import URLHandler
+import re
+from typing import Any
+
+
+class Article:
+    def __init__(
+        self,
+        title: str, 
+        date: datetime, 
+        description: str, 
+        image_src: str
+    ) -> None:
+        self.__title: str = title
+        self.__date: datetime = date
+        self.__description: str = description
+        self.__image_src: str = image_src
+
+    def get_title(self) -> str:
+        return self.__title
+
+    def get_date(self) -> datetime:
+        return self.__date
+
+    def get_description(self) -> str:
+        return self.__description
+
+    def get_image_src(self) -> str:
+        return self.__image_src
+
+    def is_before_date(self, date: datetime) -> bool:
+        return self.__date < date
+    
+    def is_between_dates(self, start_date: datetime, 
+                         end_date: datetime) -> bool:
+        return start_date <= self.__date <= end_date
+    
+    def get_image_file_name(self) -> str:
+        url_handler: URLHandler = URLHandler()
+        image_url = url_handler.get_image_url(self.__image_src)
+        image_file_name = image_url.split('/')[-1]
+        return image_file_name
+    
+    def is_contain_money_in_text(self) -> bool:
+        money_regex_pattern = (
+            r'\$\d{1,3}((,?\d{3})|(\d*))*(\.\d{1,2})?'
+            r'|(\d{1,3}(,?\d{3})*(\.\d{1,2})? (dollars|USD))'
+        )
+        match_title: Any = re.search(money_regex_pattern, self.__title)
+        match_description: Any = re.search(money_regex_pattern, 
+                                           self.__description
+                                           )
+        if match_title or match_description:
+            return True
+        return False
