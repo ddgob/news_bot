2e7ec46ac4386e72a6861e27f1e82be9c787a783
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:47:35 -0300

    Add docstrings to class and methods of NewsWebsiteBrowserHandler

--- news_bot/news_website_browser_service/news_website_browser_handler.py
+++ news_bot/news_website_browser_service/news_website_browser_handler.py
@@ -3,20 +3,71 @@ from abc import ABC, abstractmethod
 from RPA.Browser.Selenium import Selenium
 
 class NewsWebsiteBrowserHandler(ABC):
+    """
+    Abstract base class for handling browser interactions with a news 
+    website.
+
+    This class provides an interface for interacting with a news website 
+    using a browser, including opening the website, performing searches,
+    selecting articles, and closing the browser.
+    """
+
     @abstractmethod
     def open_website(self) -> None:
+        """
+        Open the news website in a browser.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a 
+            subclass.
+        """
         pass
 
     @abstractmethod
     def search(self, phrase: str) -> None:
+        """
+        Search for articles on the news website using the specified 
+        phrase.
+
+        Args:
+            phrase (str): The search phrase to use.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a 
+            subclass.
+        """
         pass
 
     @abstractmethod
     def select_newest_articles(self) -> None:
+        """
+        Select the newest articles on the news website.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a 
+            subclass.
+        """
         pass
 
     def close_browser(self) -> None:
+        """
+        Close the browser.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a 
+            subclass.
+        """
         pass
 
     def _get_handler(self) -> Selenium:
+        """
+        Get the Selenium browser handler used by this service.
+
+        Returns:
+            Selenium: The Selenium browser handler.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a 
+            subclass.
+        """
         pass
