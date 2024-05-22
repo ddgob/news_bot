f2285b214e31d76d44e563303c96879c85258578
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:47:45 -0300

    Add docstrings to class and methods of NewsWebsiteBrowserHandlerFactory

--- news_bot/news_website_browser_service/news_website_browser_handler_factory.py
+++ news_bot/news_website_browser_service/news_website_browser_handler_factory.py
@@ -4,8 +4,29 @@ from .news_website_browser_handler import NewsWebsiteBrowserHandler
 
 
 class NewsWebsiteBrowserHandlerFactory:
+    """
+    Factory class to create an instance of NewsWebsiteBrowserHandler 
+    based on the provided website URL.
+    """
+
     @staticmethod
     def create(website_url: str) -> NewsWebsiteBrowserHandler:
+        """
+        Create an instance of NewsWebsiteBrowserHandler based on the 
+        website URL.
+
+        Args:
+            website_url (str): The URL of the news website for which a 
+            browser handler is needed.
+
+        Returns:
+            NewsWebsiteBrowserHandler: An instance of a concrete 
+            subclass of NewsWebsiteBrowserHandler.
+
+        Raises:
+            ValueError: If no browser handler is available for the given
+            website URL.
+        """
         if website_url == 'https://www.latimes.com/':
             from .la_times_browser_handler import LATimesBrowserHandler
             return LATimesBrowserHandler(Selenium())
