2948767af44e140807a9b3029e432c183f954d22
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 01:55:20 -0300

    refactor: Add error handling in NewsWebsiteBrowserHandlerFactory
    - Added error handling to NewsWebsiteBrowserHandlerFactory in the case there is no available browser handler for the website that was passed as argument.
    - This improves code robustness and facilitates debugging.

--- news_bot/news_website_browser_handler_factory.py
+++ news_bot/news_website_browser_handler_factory.py
@@ -8,3 +8,7 @@ class NewsWebsiteBrowserHandlerFactory:
         if website_url == 'https://www.latimes.com/':
             from .la_times_browser_handler import LATimesBrowserHandler
             return LATimesBrowserHandler(Selenium())
+        else:
+            raise ValueError(
+                f'No browser handler available for website {website_url}'
+                )
