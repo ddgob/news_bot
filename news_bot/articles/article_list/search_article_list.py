6c443466f5b872ad38ea252af526e38051698742
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:48:26 -0300

    Add docstrings to class and methods of SearchArticleList

--- news_bot/articles/article_list/search_article_list.py
+++ news_bot/articles/article_list/search_article_list.py
@@ -5,14 +5,43 @@ from ..article import Article
 from ..article import SearchArticle
 
 class SearchArticleList(ArticleList):
+    """
+    A list to manage SearchArticle objects with additional functionality 
+    specific to search phrases.
+    """
+
     def __init__(self, search_phrase: str) -> None:
+        """
+        Initialize the SearchArticleList with a search phrase.
+
+        Args:
+            search_phrase (str): The search phrase associated with the 
+            articles in the list.
+        """
         super().__init__()
         self.__search_phrase: str = search_phrase
 
     def __iter__(self):
+        """
+        Return an iterator over the articles in the list.
+
+        Returns:
+            iterator: An iterator over the articles.
+        """
         return iter(self._get_articles())
 
     def append(self, article_to_append: Article) -> None:
+        """
+        Append an article to the list.
+
+        Args:
+            article_to_append (Article): The article to append.
+
+        Raises:
+            ValueError: If the article is not of type Article or 
+                        SearchArticle, or if the search phrase does not 
+                        match.
+        """
         article: Article = article_to_append
         if not isinstance(article, Article):
             raise ValueError(
@@ -30,10 +59,26 @@ class SearchArticleList(ArticleList):
         super().append(article)
 
     def extend(self, articles: ArticleList) -> None:
+        """
+        Extend the list by appending elements from another article list.
+
+        Args:
+            articles (ArticleList): The list of articles to append.
+
+        Returns:
+            None
+        """
         for article in articles:
             self.append(article)
 
     def convert_to_list_of_dicts(self) -> list[dict]:
+        """
+        Convert the list of articles to a list of dictionaries.
+
+        Returns:
+            list[dict]: A list of dictionaries representing the 
+            articles.
+        """
         list_of_dict: list[dict] = []
         for article in self._get_articles():
             search_article = SearchArticle.from_article(article, 
@@ -44,6 +89,17 @@ class SearchArticleList(ArticleList):
     
     def filter_articles_within_date_range(self, start_date: datetime, 
                                    end_date: datetime) -> 'SearchArticleList':
+        """
+        Filter the articles within a specific date range.
+
+        Args:
+            start_date (datetime): The start date of the range.
+            end_date (datetime): The end date of the range.
+
+        Returns:
+            SearchArticleList: A new SearchArticleList with articles 
+            within the specified date range.
+        """
         articles_within_date_range = SearchArticleList(self.__search_phrase)
         for article in self._get_articles():
             if not article.is_between_dates(start_date, end_date):
