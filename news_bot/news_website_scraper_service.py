c3f18333e33b92329c9dbfc67d58648a73c8baaf
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 02:39:27 -0300

    feat: Add NewsWebsiteScraperService class to handle article scraping
    - Introduced NewsWebsiteScraperService class to provide a high-level interface for scraping articles from news websites.
    - Integrated NewsWebsiteArticleScraperFactory to create specific scraper instances based on the website URL.
    - Implemented scrape_search_articles_within_date_range method to scrape articles within a given date range and search phrase using a provided browser handler.
    - Ensured compatibility with NewsWebsiteBrowserService for handling browser interactions.
    - Added necessary imports and constructor to initialize the service with the correct scraper.
    
    This service class simplifies the process of scraping articles by encapsulating the creation of specific scrapers and delegating the scraping task.

---
+++ news_bot/news_website_scraper_service.py
@@ -0,0 +1,21 @@
+from .news_website_article_scraper_factory import NewsWebsiteArticleScraperFactory
+from datetime import datetime
+from .news_website_browser_service import NewsWebsiteBrowserService
+from .articles.article_list import SearchArticleList
+
+
+
+class NewsWebsiteScraperService:
+    def __init__(self, website_url: str):
+        self.news_website_article_scraper = NewsWebsiteArticleScraperFactory.create(
+            website_url
+            )
+        
+    def scrape_search_articles_within_date_range(self, start_date: datetime, 
+                                                 end_date: datetime, 
+                                                 search_phrase: str,
+                                                 handler: NewsWebsiteBrowserService
+                                                 ) -> SearchArticleList:
+        return self.news_website_article_scraper.scrape_search_articles_within_date_range(
+            start_date, end_date, search_phrase, handler
+            )
