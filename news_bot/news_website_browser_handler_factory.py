281fe1c3679c15e5e6262adabf27119fb1597f65
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 01:56:55 -0300

    refactor: Update NewsWebsiteBrowserHandlerFactory to handle website URL as string
    This commit updates the `NewsWebsiteBrowserHandlerFactory` class to handle the website URL as a string parameter. This change improves code clarity and ensures consistent data type handling.

--- news_bot/news_website_browser_handler_factory.py
+++ news_bot/news_website_browser_handler_factory.py
@@ -4,7 +4,7 @@ from RPA.Browser.Selenium import Selenium
 
 class NewsWebsiteBrowserHandlerFactory:
     @staticmethod
-    def create(website_url) -> NewsWebsiteBrowserHandler:
+    def create(website_url: str) -> NewsWebsiteBrowserHandler:
         if website_url == 'https://www.latimes.com/':
             from .la_times_browser_handler import LATimesBrowserHandler
             return LATimesBrowserHandler(Selenium())
