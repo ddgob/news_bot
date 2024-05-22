5d50183eb6541187088f1181ae7affc156e5d3a0
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:47:20 -0300

    Add docstrings to class and methods of NewsWebsiteBrowserService

--- news_bot/news_website_browser_service/news_website_browser_service.py
+++ news_bot/news_website_browser_service/news_website_browser_service.py
@@ -1,22 +1,67 @@
 from .news_website_browser_handler_factory import NewsWebsiteBrowserHandlerFactory
 
 class NewsWebsiteBrowserService:
+    """
+    Service class for handling browser interactions with a news website.
+    """
+
     def __init__(self, website_url: str):
+        """
+        Initialize the NewsWebsiteBrowserService.
+
+        Args:
+            website_url (str): The URL of the news website to interact 
+            with.
+        """
         self.news_website_browser_handler = NewsWebsiteBrowserHandlerFactory.create(
             website_url
             )
         
     def open_website(self) -> None:
+        """
+        Open the news website in a browser.
+
+        Returns:
+            None
+        """
         self.news_website_browser_handler.open_website()
 
     def search(self, phrase: str) -> None:
+        """
+        Search for articles on the news website using the specified 
+        phrase.
+
+        Args:
+            phrase (str): The search phrase to use.
+
+        Returns:
+            None
+        """
         self.news_website_browser_handler.search(phrase)
 
     def select_newest_articles(self) -> None:
+        """
+        Select the newest articles on the news website.
+
+        Returns:
+            None
+        """
         self.news_website_browser_handler.select_newest_articles()
 
     def close_browser(self) -> None:
+        """
+        Close the browser.
+
+        Returns:
+            None
+        """
         self.news_website_browser_handler.close_browser()
 
     def _get_handler(self):
+        """
+        Get the browser handler used by this service.
+
+        Returns:
+            The browser handler used by this service.
+        """
         return self.news_website_browser_handler._get_handler()
