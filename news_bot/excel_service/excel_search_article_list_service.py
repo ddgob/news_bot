7ac6861139465a4d6f328f90cef5e1bca3e88521
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:48:08 -0300

    Add docstrings to class and methods of ExcelSearchArticleListService

--- news_bot/excel_service/excel_search_article_list_service.py
+++ news_bot/excel_service/excel_search_article_list_service.py
@@ -3,7 +3,17 @@ from .excel_handler import ExcelHandler
 
 
 class ExcelSearchArticleListService:
+    """
+    Service class for saving a list of search articles to an Excel file.
+    """
+
     def __init__(self) -> None:
+        """
+        Initialize the ExcelSearchArticleListService.
+
+        Sets up the Excel handler for converting and saving articles to 
+        an Excel file.
+        """
         self.__excel_handler = ExcelHandler()
 
 
@@ -11,6 +21,20 @@ class ExcelSearchArticleListService:
                                                articles: SearchArticleList, 
                                                excel_file_name: str, 
                                                worksheet_name: str) -> None:
+        """
+        Save the search article list to an Excel file.
+
+        Args:
+            articles (SearchArticleList): The list of search articles to
+            save.
+            excel_file_name (str): The name of the Excel file to save 
+            the articles to.
+            worksheet_name (str): The name of the worksheet within the 
+            Excel file.
+
+        Returns:
+            None
+        """
         list_of_dict_articles = articles.convert_to_list_of_dicts()
         self.__excel_handler.convert_list_of_dicts_to_excel_file(
             list_of_dict_articles, excel_file_name, worksheet_name
