4bd58b1bd8ced8bcf457cd6efba7cd027072e4e6
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 03:02:27 -0300

    feat: Add ExcelSearchArticleListService class for exporting articles to Excel
    - Introduced ExcelSearchArticleListService class to facilitate exporting articles to an Excel file.
    - Integrated ExcelHandler to handle the conversion of article data to Excel format.
    - Implemented save_search_article_list_to_excel_file method to convert SearchArticleList to a list of dictionaries and save it to an Excel file.
    - Added necessary imports and constructor to initialize the Excel handler.
    
    This service class provides a convenient way to save search results to an Excel file, enhancing data portability and reporting capabilities.

---
+++ news_bot/excel_search_article_list_service.py
@@ -0,0 +1,17 @@
+from .articles.article_list import SearchArticleList
+from .excel_handler import ExcelHandler
+
+
+class ExcelSearchArticleListService:
+    def __init__(self) -> None:
+        self.__excel_handler = ExcelHandler()
+
+
+    def save_search_article_list_to_excel_file(self, 
+                                               articles: SearchArticleList, 
+                                               excel_file_name: str, 
+                                               worksheet_name: str) -> None:
+        list_of_dict_articles = articles.convert_to_list_of_dicts()
+        self.__excel_handler.convert_list_of_dicts_to_excel_file(
+            list_of_dict_articles, excel_file_name, worksheet_name
+            )
