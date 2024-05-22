8d3b9c70361c6576a5fef49a9e40b55ab87566e4
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 14:47:16 -0300

    refactor: Update SearchArticle class to include method for counting search phrase occurrences
    - This commit updates the SearchArticle class to include a new method, get_search_phrase_count(), which calculates the number of occurrences of the search phrase in the article's title and description.
    - The method converts the search phrase, title, and description to lowercase and uses the count() method to count the occurrences. The total count is then returned.
    
    This change enhances the functionality of the SearchArticle class by providing a convenient way to retrieve the count of search phrase occurrences in the article text.

--- news_bot/articles/article/search_article.py
+++ news_bot/articles/article/search_article.py
@@ -26,6 +26,14 @@ class SearchArticle(Article):
     def get_search_phrase(self) -> str:
         return self.__search_phrase
     
+    def get_search_phrase_count(self) -> str:
+        search_phrase = self.get_search_phrase().lower()
+        title = self.get_title().lower()
+        description = self.get_description().lower()
+        title_count = title.count(search_phrase)
+        description_count = description.count(search_phrase)
+        return title_count + description_count
+    
     def convert_to_dict(self) -> dict:
         date_handler: DateHandler = DateHandler()
         string_date: str = date_handler.convert_datetime_to_string(
@@ -36,6 +44,6 @@ class SearchArticle(Article):
             'Date': string_date,
             'Description': self.get_description(),
             'Image file name': self.get_image_file_name(),
-            'Search phrase count in article text': self.get_search_phrase(),
+            'Search phrase count in article text': self.get_search_phrase_count(),
             'Is article text contain money': self.is_contain_money_in_text()
         }
