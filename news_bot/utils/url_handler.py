6f65d3a5567744fa8be07fbc5a28fdc07895652b
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:44:55 -0300

    Add docstrings to class and methods of URLHandler

--- news_bot/utils/url_handler.py
+++ news_bot/utils/url_handler.py
@@ -5,10 +5,33 @@ from .logger import Logger
 
 
 class URLHandler:
+    """
+    Handles URL parsing and extraction tasks.
+    """
+
     def __init__(self) -> None:
+        """
+        Initialize the URLHandler.
+
+        Sets up the logger for logging activities.
+        """
         self.__log = Logger().log
 
     def get_image_url(cls, image_src) -> str:
+        """
+        Extract the image URL from a given image source URL.
+
+        Args:
+            image_src (str): The source URL containing the image URL as 
+            a query parameter.
+
+        Returns:
+            str: The extracted image URL.
+
+        Raises:
+            ValueError: If the image URL cannot be extracted from the 
+            source URL.
+        """
         parsed_url: Any = urlparse(image_src)
         query_params: Any = parse_qs(parsed_url.query)
         image_url: Any = query_params.get('url', [None])[0]
