20d7f2cc8c3b543bc661b9b27df6e191a71b08f2
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 20:46:58 -0300

    Add docstrings to class and methods of NewsWebsiteArticleScraperFactory

--- news_bot/news_website_scraper_service/news_website_article_scraper_factory.py
+++ news_bot/news_website_scraper_service/news_website_article_scraper_factory.py
@@ -2,8 +2,25 @@ from .news_website_article_scraper import NewsWebsiteArticleScraper
 
 
 class NewsWebsiteArticleScraperFactory:
+    """
+    Factory class to create an instance of NewsWebsiteArticleScraper 
+    based on the provided website URL.
+    """
+
     @staticmethod
     def create(website_url: str) -> NewsWebsiteArticleScraper:
+        """
+        Create an instance of NewsWebsiteArticleScraper based on the website URL.
+
+        Args:
+            website_url (str): The URL of the news website for which an article scraper is needed.
+
+        Returns:
+            NewsWebsiteArticleScraper: An instance of a concrete subclass of NewsWebsiteArticleScraper.
+
+        Raises:
+            ValueError: If no article scraper is available for the given website URL.
+        """
         if website_url == 'https://www.latimes.com/':
             from .la_times_article_scraper import LATimesArticleScraper
             return LATimesArticleScraper()
