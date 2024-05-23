10cd4a4dcdff6f44db1474e629dd6a47eccc4a1c
Daniel Barbosa <danielolibar@gmail.com>
2024-05-22 21:19:58 -0300

    chore: Improve type hints (fix errors shown by mypy)

--- news_bot/news_bot.py
+++ news_bot/news_bot.py
@@ -51,14 +51,16 @@ class NewsBot:
         """
         try:
             date_handler = DateHandler()
-            start_date, end_date = date_handler.first_date_earlier_than_second(
+            datetime_start_date, datetime_end_date = date_handler.first_date_earlier_than_second(
                 start_date, end_date
                 )
-            end_date = date_handler.make_until_end_of_day(end_date)
+            datetime_end_date = date_handler.make_until_end_of_day(
+                datetime_end_date
+                )
             self.__log(
                 'info', 
-                f'Scraping articles within dates {start_date} and {end_date} '
-                f'for search phrase: {search_phrase}...'
+                f'Scraping articles within dates {datetime_start_date} and '
+                f'{datetime_end_date} for search phrase: {search_phrase}...'
                 )
             news_website_browser_service = NewsWebsiteBrowserService(
                 website_url
@@ -70,27 +72,29 @@ class NewsBot:
                 website_url
             )
             articles = news_website_scraper_service.scrape_search_articles_within_date_range(
-                start_date, end_date, search_phrase, 
+                datetime_start_date, datetime_end_date, search_phrase, 
                 news_website_browser_service
                 )
             news_website_browser_service.close_browser()
             excel_search_article_list_service = ExcelSearchArticleListService()
-            formatted_start_date = date_handler.convert_datetime_to_string(
-                start_date, separator='-'
+            formatted_datetime_start_date = date_handler.convert_datetime_to_string(
+                datetime_start_date, separator='-'
                 )
-            formatted_end_date = date_handler.convert_datetime_to_string(
-                end_date, separator='-'
+            formatted_datetime_end_date = date_handler.convert_datetime_to_string(
+                datetime_end_date, separator='-'
                 )
             worksheet_name = (
-                f'{search_phrase}_{formatted_start_date}_{formatted_end_date}'
+                f'{search_phrase}_{formatted_datetime_start_date}_'
+                f'{formatted_datetime_end_date}'
             )
             excel_search_article_list_service.save_search_article_list_to_excel_file(
                 articles, excel_files_dir, worksheet_name
                 )
             self.__log(
                 'info', 
-                f'Finished scraping articles within dates {start_date} and '
-                f'{end_date} for search phrase: {search_phrase}'
+                f'Finished scraping articles within dates '
+                f'{datetime_start_date} and {datetime_end_date} for search '
+                f'phrase: {search_phrase}'
                 )
             image_downloader = ImageDownloader()
             image_downloader.download_images(articles, images_dir)
@@ -99,7 +103,7 @@ class NewsBot:
             self.__log(
                 'error', 
                 f'An error occured while scraping articles within dates '
-                f'{start_date} and {end_date} for search phrase: '
-                f'{search_phrase}'
+                f'{datetime_start_date} and {datetime_end_date} for search '
+                f'phrase: {search_phrase}'
                 )
             return False
